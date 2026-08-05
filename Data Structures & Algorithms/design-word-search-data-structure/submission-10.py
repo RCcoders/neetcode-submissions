ffe3465510aa3b42fe63b8812e97182ebae8433d
Raghav Chawla <cec235524.aiml.cec@cgc.edu.in>
2026-08-05 23:57:27 +0530

    Add: design-word-search-data-structure - submission-10

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-10.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+        node.is_end_of = True
+
+    def search(self, word: str) -> bool:
+        def dfs(index, node):
+            if index == len(word):
+                return node.is_end_of
+
+            char = word[index]
+            if char == ".":
+                for child in node.children.values():
+                    if dfs(index + 1, child):
+                        return True
+                return False
+
+            if char not in node.children:
+                return False
+            return dfs(index + 1, node.children[char])
+        return dfs(0, self.root)
